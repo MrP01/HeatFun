fe0421f6397493f03633928b86c6b89a4a35fed5
MrP01 <peter@waldert.at>
2023-01-03 02:06:36 +0100

    preliminarily finish report

--- solver/TschebFun.h
+++ solver/TschebFun.h
@@ -16,13 +16,14 @@ class TschebFun {
   size_t degree() { return coefficients.size() - 1; };
   Vector evaluateOn(Vector x);
   TschebFun derivative();
-  TschebFun operator+(const TschebFun &other);
-  TschebFun operator-(const TschebFun &other);
-  TschebFun operator*(const double &factor);
   static TschebFun interpolantThrough(Vector y);
 
   static Vector equipoints(size_t N);
   static Vector chebpoints(size_t N);
   static Vector modifiedEquipoints(size_t N);
   static Vector modifiedChebpoints(size_t N);
+
+  TschebFun operator+(const TschebFun &other);
+  TschebFun operator-(const TschebFun &other);
+  TschebFun operator*(const double &factor);
 };
