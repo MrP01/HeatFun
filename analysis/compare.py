131cfaa6764f9009fe532953b149e6d04dfbf099
MrP01 <peter@waldert.at>
2023-01-10 12:48:58 +0000

    improvement suggestions from Yuji! :)

--- analysis/compare.py
+++ analysis/compare.py
@@ -7,6 +7,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tikzplotlib
 
+
+def monkeypatch_matplotlib():
+    from matplotlib.lines import Line2D
+    from matplotlib.legend import Legend
+
+    Line2D._us_dashSeq = property(lambda self: self._dash_pattern[1])
+    Line2D._us_dashOffset = property(lambda self: self._dash_pattern[0])
+    Legend._ncol = property(lambda self: self._ncols)
+
+
+monkeypatch_matplotlib()
+
 BASE = pathlib.Path(__file__).parent.parent
 RESULTS = BASE / "report" / "figures"
 sys.path.append(str(BASE / "build" / "lib"))
@@ -22,7 +34,8 @@ INTERESTING_U0 = {
 X_TO_PLOT = np.linspace(-1.0, 1.0, 500)
 ORDER_OF_INTERPOLATION = 30
 
-# plt.style.use("ggplot")
+plt.style.use("seaborn-v0_8-muted")
+STYLE_COLORS = list(plt.rcParams["axes.prop_cycle"])
 
 
 def heatfun_solution(example_key: str):
@@ -52,32 +65,33 @@ def matlab_solution(example_key: str):
 
 
 def analyse(example_key: str):
+    u0 = heatfun.evaluateExpression(INTERESTING_U0[example_key], X_TO_PLOT)
     our_solution = heatfun_solution(example_key)
     chebfun_solution = matlab_solution(example_key)
     print(example_key, "squared error:", sum((our_solution - chebfun_solution) ** 2))
 
     fig = plt.figure()
     axes: matplotlib.axes.Axes = fig.add_subplot(2, 1, 1)
-    axes.plot(X_TO_PLOT, heatfun.evaluateExpression(INTERESTING_U0[example_key], X_TO_PLOT), label="$u(x, 0) = u_0(x)$")
-    axes.plot(X_TO_PLOT, our_solution, label="$u_{heatfun}(x, T)$")
-    axes.plot(X_TO_PLOT, chebfun_solution, label="$u_{chebfun}(x, T)$")
+    axes.plot(X_TO_PLOT, u0, c=STYLE_COLORS[0]["color"], label="$u_0(x)$")
+    axes.plot(X_TO_PLOT, chebfun_solution, c="k", label="$u_{cheb}(x, T)$")
+    axes.plot(X_TO_PLOT, our_solution, "--", c=STYLE_COLORS[1]["color"], label="$u_{heat}(x, T)$")
     axes.set_ylabel("$u(x)$")
     axes.legend()
     axes: matplotlib.axes.Axes = fig.add_subplot(2, 1, 2)
-    axes.semilogy(X_TO_PLOT, abs(our_solution - chebfun_solution), "m")
+    axes.semilogy(X_TO_PLOT, abs(our_solution - chebfun_solution), c=STYLE_COLORS[3]["color"])
     axes.set_xlabel("$x$")
-    axes.set_ylabel("$|u_{heat}(x, T) - u_{cheb}(x, T)|$")
+    axes.set_ylabel("$|u_{heat} - u_{cheb}|$")
     tikzplotlib.save(
         RESULTS / f"comparison-{example_key}.tex",
         figure=fig,
         axis_width=r"0.8\linewidth",
-        axis_height=r"0.3\linewidth",
+        axis_height=r"0.25\linewidth",
     )
     fig.savefig(str(RESULTS / f"comparison-{example_key}.png"))
 
 
 if __name__ == "__main__":
-    all_matlab_solutions()
+    # all_matlab_solutions()
     for key in INTERESTING_U0.keys():
         analyse(key)
     plt.show()
